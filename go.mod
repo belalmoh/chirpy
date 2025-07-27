cc7961d7518b30af16fa743842442c74c968289b
Belal Mohammed <belmoe94@gmail.com>
2025-07-27 20:49:48 +0400

    Initialize project with Go module, main server implementation, and .gitignore

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/belalmoh/chirpy
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := "8080"
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %v", port)
+	log.Fatal(server.ListenAndServe())
+
+}
